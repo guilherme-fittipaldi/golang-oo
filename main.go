ca38c5222aea505e31bc96dd24271eea9fbfe8e4
unknown <guilherme.fittipaldi6@gmail.com>
2021-09-08 09:50:49 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import( 
+	"fmt"
+	
+)
+
+
+func main() {
+
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 300
+	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+
+	status := contaDoGustavo.Transferir(-200, &contaDaSilvia)
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
+
+	// contaDoGuilherme := ContaCorrente{titular: "Guilherme",
+	// 	numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	// contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	// fmt.Println(contaDoGuilherme)
+	// fmt.Println(contaDaBruna)
+
+	// var contaDaCris *ContaCorrente
+	// contaDaCris = new(ContaCorrente)
+	// contaDaCris.titular = "Cris"
+	// contaDaCris.saldo = 500
+
+	// fmt.Println(*contaDaCris)
+}
